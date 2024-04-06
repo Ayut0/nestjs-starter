ad80bc9b4f39577aab546a4fcb329f1b20c4de84
Yuto Yama <yuvto.0420@gmail.com>
2024-04-06 15:15:26 -0700

    Modify controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+// import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DogsModule } from './dogs/dogs.module';
+import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [DogsModule],
-  controllers: [AppController],
+  controllers: [DogsController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/dogs/dogs.controller.ts
+++ src/dogs/dogs.controller.ts
@@ -3,34 +3,70 @@ import {
   Get,
   Post,
   Body,
-  Patch,
   Param,
   Delete,
+  Query,
+  Put,
 } from '@nestjs/common';
 import { DogsService } from './dogs.service';
-import { CreateDogDto } from './dto/create-dog.dto';
-import { UpdateDogDto } from './dto/update-dog.dto';
+// import { CreateDogDto } from './dto/create-dog.dto';
+import { UpdateDogDto, ListAllEntities } from './dto/update-dog.dto';
+// import type { Request } from 'express';
+
+export class CreateDogDto {
+  name: string;
+  breed: string;
+  age: number;
+}
 
 @Controller('dogs')
 export class DogsController {
   constructor(private readonly dogsService: DogsService) {}
 
+  // @Post()
+  // create(@Body() createDogDto: CreateDogDto) {
+  //   return this.dogsService.create(createDogDto);
+  // }
+
+  // @Post()
+  // create() {
+  //   return 'This action adds a new dog';
+  // }
+
   @Post()
-  create(@Body() createDogDto: CreateDogDto) {
-    return this.dogsService.create(createDogDto);
+  async create(@Body() createDogDto: CreateDogDto) {
+    return `This action adds a new dog ${createDogDto.name}`;
   }
 
-  @Get()
-  findAll() {
-    return this.dogsService.findAll();
-  }
+  // @Get()
+  // @Redirect('https://nestjs.com', 301)
+  // findAll() {
+  //   return this.dogsService.findAll();
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.dogsService.findOne(+id);
   }
 
-  @Patch(':id')
+  @Get()
+  findAll(@Query() query: ListAllEntities) {
+    return `This action returns all dogs (limit: ${query.limit} items)`;
+  }
+
+  // Route parameters
+  // @Get(':id')
+  // findOne(@Param('id') id: string): string {
+  //   return `This action returns a #${id} dog`;
+  // }
+
+  // Route wildcards
+  // @Get('ab*cd')
+  // findAll() {
+  //   return 'This route uses a wildcard';
+  // }
+
+  @Put(':id')
   update(@Param('id') id: string, @Body() updateDogDto: UpdateDogDto) {
     return this.dogsService.update(+id, updateDogDto);
   }
