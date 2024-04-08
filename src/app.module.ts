c43e7f7e4d57c6b97c26174b6290c1278d6cda76
Yuto Yama <yuvto.0420@gmail.com>
2024-04-08 16:38:59 -0700

    Add middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { LoggerMiddleware } from './middleware/logger.middleware';
 import { DogsModule } from './dogs/dogs.module';
+import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [DogsModule],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(DogsController);
+  }
+}
