b9d54ba35d7ea1cdd4147a4a4b5b2ec389e6876a
Yuto Yama <yuvto.0420@gmail.com>
2024-04-08 14:48:12 -0700

    Edit modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DogsModule } from './dogs/dogs.module';
-import { DogsController } from './dogs/dogs.controller';
 
 @Module({
   imports: [DogsModule],
-  controllers: [DogsController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/dogs/dogs.module.ts
+++ src/dogs/dogs.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { DogsService } from './dogs.service';
 import { DogsController } from './dogs.controller';
 
+@Global()
 @Module({
   controllers: [DogsController],
   providers: [DogsService],
+  exports: [DogsService],
 })
-export class DogsModule {}
+export class DogsModule {
+  constructor(private dogsService: DogsService) {}
+}
